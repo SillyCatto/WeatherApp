78b6cb44355cb6151f61ea51dee10d206fa066cc
musaddiq-rafi <musaddiq@iut-dhaka.edu>
2024-11-26 21:01:34 +0600

    gg

--- App/src/App.jsx
+++ App/src/App.jsx
@@ -1,11 +1,30 @@
-import React from "react";
+import React, {useState} from "react";
 import ReactDOM from "react-dom/client";
 
 const App = () => {
     return (
-      <h1>Hello World!</h1>
+      <div>
+        <h1>Hello World!</h1>
+        <p>My first React App</p>
+        <MyButton></MyButton>
+        <MyButton></MyButton>
+      </div>
     );
 };
+function MyButton() {
+  const [count, setCount] = useState(0);
+
+  function handleClick() {
+    setCount(count + 1);
+  }
+
+  return (
+    <button onClick={handleClick}>
+      Clicked {count} times
+    </button>
+  );
+}
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App/>);
