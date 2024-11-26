36162866f8b2a264144e61618dfc24d1b3574677
musaddiq-rafi <musaddiq@iut-dhaka.edu>
2024-11-26 22:10:03 +0600

    .

--- App/src/App.jsx
+++ App/src/App.jsx
@@ -1,13 +1,17 @@
 import React, {useState} from "react";
 import ReactDOM from "react-dom/client";
 
+
 const App = () => {
     return (
       <div>
-        <h1>Hello World!</h1>
-        <p>My first React App</p>
-        <MyButton></MyButton>
-        <MyButton></MyButton>
+      <div>
+      <h1>Hello World!</h1>
+      <p>Tic Tac Toe </p>
+      <MyButton />
+      <MyButton />
+      </div>
+      <Board />
       </div>
     );
 };
@@ -22,6 +26,33 @@ function MyButton() {
     <button onClick={handleClick}>
       Clicked {count} times
     </button>
+  );
+}
+
+function Square() {
+  return <button className="square">1</button>;
+}
+
+export default function Board() {
+  return(
+  <>
+  <div className="board-row">
+    <button className="square">1</button>
+    <button className="square">2</button>
+    <button className="square">3</button>
+  </div>
+  <div classname="board-row">
+  <button className="square">4</button>
+  <button className="square">5</button>
+  <button className="square">6</button>
+  </div>
+  <div className="board-row">
+  <button className="square">7</button>
+  <button className="square">8</button>
+  <button className="square">9</button>
+  </div>
+  
+  </>
   );
 }
 
