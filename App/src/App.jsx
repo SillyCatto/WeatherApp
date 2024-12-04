81056f32885281bcde7c86b8e60e9e6283ba9e4b
SillyCatto <raiyanmuhtasim@gmail.com>
2024-12-04 20:39:04 +0600

    added styling to board

--- App/src/App.jsx
+++ App/src/App.jsx
@@ -6,29 +6,28 @@ import ReactDOM from "react-dom/client";
 const App = () => {
   return (
     <div>
-      <div>
-        <h1>Hello World!</h1>
-        <p>Tic Tac Toe </p>
-        <MyButton />
-        <MyButton />
+      <div className="flex flex-col items-center justify-center p-4" >
+        <p className="font-bold text-4xl" >Tic Tac Toe </p>
+        <p className="p-2 font-medium" >Turn: Player-1</p>
       </div>
       <Board />
     </div>
   );
 };
-function MyButton() {
-  const [count, setCount] = useState(0);
 
-  function handleClick() {
-    setCount(count + 1);
-  }
+// function MyButton() {
+//   const [count, setCount] = useState(0);
 
-  return (
-    <button onClick={handleClick}>
-      Clicked {count} times
-    </button>
-  );
-}
+//   function handleClick() {
+//     setCount(count + 1);
+//   }
+
+//   return (
+//     <button onClick={handleClick}>
+//       Clicked {count} times
+//     </button>
+//   );
+// }
 
 function Square() {
   const [value, setValue] = useState(null);
@@ -47,21 +46,23 @@ function Square() {
 export default function Board() {
   return (
     <>
-      <div className="flex flex-col gap-2" >
-        <div className="board-row flex gap-2">
-          <Square />
-          <Square />
-          <Square />
-        </div>
-        <div className="board-row flex gap-2">
-          <Square />
-          <Square />
-          <Square />
-        </div>
-        <div className="board-row flex gap-2">
-          <Square />
-          <Square />
-          <Square />
+      <div className="flex justify-center items-center p-6" >
+        <div className="flex flex-col gap-2" >
+          <div className="board-row flex gap-2">
+            <Square />
+            <Square />
+            <Square />
+          </div>
+          <div className="board-row flex gap-2">
+            <Square />
+            <Square />
+            <Square />
+          </div>
+          <div className="board-row flex gap-2">
+            <Square />
+            <Square />
+            <Square />
+          </div>
         </div>
       </div>
     </>
